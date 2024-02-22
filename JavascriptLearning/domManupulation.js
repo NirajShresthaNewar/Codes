a13844ec0d373cff26c9cc5fb45d73270cf1fc90
NirajShresthaNewar <nirojs022@gmail.com>
2024-02-22 20:45:44 +0545

    er

---
+++ JavascriptLearning/domManupulation.js
@@ -0,0 +1,18 @@
+document.getElementById('title')//gives the elements uith id title
+document.getElementById('title').id//gives the name of id
+document.getElementById('title').class//it wont give name of class .gives undefined
+document.getElementById('title').className//it  give name of class because class is not recognizd as seen in above.
+
+document.getElementById('title').getAttribute//this willgive function
+document.getElementById('title').getAttribute('id')//you can ask for attribute likethis
+document.getElementById('title').getAttribute('class')//itwill giveclass name asyoucan see abovehere yoccan get class name with out writingclassName
+
+document.getElementById('title').setAttribute('class','heading test')//here bothheading and test is the class attribute 
+document.getElementById('title').setAttribute('class','test')//here class heading is override byt test. 
+
+
+
+
+
+
+
